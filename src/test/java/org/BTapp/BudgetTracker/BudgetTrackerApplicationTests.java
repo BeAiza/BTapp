2eed8712ba2099d4e53d54b419a934ba4d1d7376
Belmonte's pc <belmontei9595@gmail.com>
2023-04-08 20:56:49 -0500

    Unversioned Files

---
+++ src/test/java/org/BTapp/BudgetTracker/BudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.BTapp.BudgetTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
