50997f8e268892a9ea27ce0e76adeb57c2328530
Belmonte's pc <belmontei9595@gmail.com>
2023-04-09 10:58:49 -0500

    Adjusting .gradle and adding js for front end

---
+++ src/main/resources/static/styles.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // Login button click event handler
+    $("#loginButton").on("click", function() {
+        alert("Login button clicked!");
+    });
+
+    // Register button click event handler
+    $("#registerButton").on("click", function() {
+        alert("Register button clicked!");
+    });
+});
