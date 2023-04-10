9888ac7f268173f6cd139c416a99a8c538ffb198
Belmonte's pc <belmontei9595@gmail.com>
2023-04-09 20:03:17 -0500

    fixing mapping

--- src/main/java/org/BTapp/BudgetTracker/controller/UserController.java
+++ src/main/java/org/BTapp/BudgetTracker/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
 
     @Autowired
